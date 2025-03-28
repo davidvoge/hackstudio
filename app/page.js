d0f10be80390bb99b1dad845f538159a2fe7fae9
Petit Hack <david@petithack.com>
2025-03-28 15:19:29 +0100

    update dependencies: upgrade Next.js to 14.2.26 and add DaisyUI; enhance global styles and page layout

--- app/page.js
+++ app/page.js
@@ -1,5 +1,24 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <h1>HackStudio</h1>;
+  return (
+    <>
+      <section className="bg-secondary h-screen">
+        <div className="flex flex-col items-center justify-center h-full mx-auto max-w-5xl">
+          <div className="card bg-base-100 w-96 shadow-sm p-4">
+            <h1>HackStudio</h1>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">
+                Donne moi ton email putain
+              </legend>
+              <input
+                type="email"
+                className="input"
+                placeholder="Ton putain d'email"
+              />
+              <p className="fieldset-label">On va te spammer !</p>
+            </fieldset>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
